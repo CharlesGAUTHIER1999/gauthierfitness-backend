5b13ed5a7022dfcfc0254927965908d650b41cf1
Charles Gauthier <charles.gauthier99@gmail.com>
2026-06-26 17:28:10 +0200

    Release GF24-Sentry -> main

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Routing\Middleware\SubstituteBindings;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,5 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => AdminMiddleware::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {})
+    ->withExceptions(function (Exceptions $exceptions) {
+        Integration::handles($exceptions);
+    })
     ->create();
